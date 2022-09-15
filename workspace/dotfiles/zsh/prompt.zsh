cfa83460166e05e9fd13d5261b6357980c1934e0
William Blasko <wblasko@twitter.com>
2022-09-15 13:56:17 -0700

    update zsh prompt
    simplify the git branch logic and add colors

--- workspace/dotfiles/zsh/prompt.zsh
+++ workspace/dotfiles/zsh/prompt.zsh
@@ -1,52 +1,11 @@
-setopt prompt_subst # enable command substition in prompt
+# enable command substition in prompt
+setopt prompt_subst 
 
-NEWLINE=$'\n'
-PROMPT='${NEWLINE}%n@%m${git_branch_name} %~${NEWLINE}# '
-RPROMPT='' # no initial prompt, set dynamically
+# enables use of `$fg_bold[green]` and $reset_color etc
+# see `which colors` for more
+autoload -Uz colors && colors
 
+source "$(dirname "$0")/git-prompt.sh"
 
-# Show Git branch/tag, or name-rev if on detached head
-function parse_git_branch() {
-  (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
-}
-
-
-# Asynchronously update RPROMPT in case the process takes a while
-# https://www.anishathalye.com/2015/02/07/an-asynchronous-shell-prompt/
-
-function rpromptcmd() {
-  parse_git_branch
-}
-
-ASYNC_PROC=0
-function precmd() {
-  function async() {
-    # save to temp file
-    printf "%s" "$(rpromptcmd)" > "/tmp/zsh_prompt_$$"
-
-    # signal parent
-    kill -s USR1 $$
-  }
-
-  RPROMPT="[?$RPROMPT?]"
-
-  # kill child if necessary
-  if [[ "${ASYNC_PROC}" != 0 ]]; then
-    kill -s HUP $ASYNC_PROC >/dev/null 2>&1 || :
-  fi
-
-  # start background computation
-  async &!
-  ASYNC_PROC=$!
-}
-
-function TRAPUSR1() {
-  # read from temp file
-  RPROMPT="$(cat /tmp/zsh_prompt_$$)"
-
-  # reset proc number
-  ASYNC_PROC=0
-
-  # redisplay
-  zle && zle reset-prompt
-}
+local NEWLINE=$'\n'
+PROMPT='${NEWLINE}$fg[blue]%n@%m$reset_color $bg[green] %~ $reset_color$fg[yellow]$(__git_ps1 " (%s)")$reset_color ${NEWLINE}# '
