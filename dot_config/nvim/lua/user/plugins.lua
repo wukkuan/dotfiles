e497be82b5948d37bcdb73a57e92a833ed8789dc
William Blasko <william@williamblasko.com>
2022-09-30 11:03:52 -0700

    use illuminate

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -151,6 +151,8 @@ return packer.startup(function(use)
     end,
   })
 
+  use('RRethy/vim-illuminate')
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
