59379cf857fbf8d4f3d70382974372c70ae13392
William Blasko <wblasko@twitter.com>
2022-10-03 19:31:47 -0700

    improve bufferline settings and shorten prefix for web

--- dot_config/nvim/lua/user/bufferline.lua
+++ dot_config/nvim/lua/user/bufferline.lua
@@ -1,5 +1,31 @@
+-- Remove vowels in the middle of the name to shorten it a bit
+local function shortenName(name)
+  if #name < 5 then
+    return name
+  end
+
+  local first = name:sub(1, 1)
+  local middle = name:sub(2, #name - 1)
+  local last = name:sub(#name)
+
+  return first .. middle:gsub('[aeiou]', '') .. last
+end
+
 require('bufferline').setup({
   options = {
+    close_command = 'BDelete %d',
+    right_mouse_command = 'BDelete %d',
+    diagnostics = true,
+    offsets = {
+      -- If NvimTree is open, keep the buffer line only over the buffers and
+      -- not the NvimTree window.
+      {
+        filetype = 'NvimTree',
+        text = '',
+        padding = 1,
+      },
+    },
+    max_name_length = 30,
     name_formatter = function(buf)
       local in_web_repo = string.match(vim.fn.getcwd(), '/workspace/web$')
         ~= nil
@@ -13,7 +39,7 @@ require('bufferline').setup({
         local parent_directory = string.match(buf.path, '/(%w*)/[^/]*$')
         -- Add some context to for each tab
         local prefix = parent_directory ~= nil
-            and ('[' .. parent_directory .. ']')
+            and ('[' .. shortenName(parent_directory) .. ']')
           or ''
 
         if buf.name == 'index.js' then
