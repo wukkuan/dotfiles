2a67d5a52fa2a7d706d020a008a43e6923ba1b0c
William Blasko <wblasko@twitter.com>
2022-10-03 19:32:44 -0700

    add trouble keymap

--- dot_config/nvim/lua/user/trouble.lua
+++ dot_config/nvim/lua/user/trouble.lua
@@ -1,3 +1,10 @@
 local trouble = require('trouble')
 
 trouble.setup({})
+
+local wk = require('which-key')
+wk.register({
+  p = {
+    q = { '<cmd>Trouble<cr>', 'Trouble' },
+  },
+}, { prefix = '<leader>' })
