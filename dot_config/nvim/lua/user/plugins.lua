3827cc1db5dc89c2d24c054f54574d3a53877dd4
William Blasko <wblasko@twitter.com>
2022-10-07 14:38:40 -0700

    Add LuaSnip

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -87,7 +87,7 @@ return packer.startup(function(use)
   use('hrsh7th/cmp-nvim-lsp')
 
   -- snippets
-  --use "L3MON4D3/LuaSnip" --snippet engine
+  use({ 'L3MON4D3/LuaSnip', run = 'make install_jsregexp', tag = 'v1.*' })
   --use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
