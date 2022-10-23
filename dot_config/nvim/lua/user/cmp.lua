5c70904ed4df1728f7b115dfd674cdb6b1a724dd
William Blasko <william@williamblasko.com>
2022-10-23 13:02:29 -0700

    add more stuff to cmp

--- dot_config/nvim/lua/user/cmp.lua
+++ dot_config/nvim/lua/user/cmp.lua
@@ -1,10 +1,5 @@
 local cmp = require('cmp')
 cmp.setup({
-  --  snippet = {
-  --    expand = function(args)
-  --      luasnip.lsp_expand(args.body)
-  --    end,
-  --  },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -16,8 +11,6 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      --      elseif luasnip.expand_or_jumpable() then
-      --        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -25,8 +18,6 @@ cmp.setup({
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      --      elseif luasnip.jumpable(-1) then
-      --        luasnip.jump(-1)
       else
         fallback()
       end
@@ -34,6 +25,9 @@ cmp.setup({
   }),
   sources = {
     { name = 'nvim_lsp' },
-    --    { name = 'luasnip' },
+    { name = 'nvim_lua' },
+    { name = 'path' },
+    { name = 'buffer' },
+    { name = 'cmdline' },
   },
 })
--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -80,11 +80,12 @@ return packer.startup(function(use)
 
   -- cmp plugins
   use('hrsh7th/nvim-cmp') -- The completion plugin
-  --use "hrsh7th/cmp-buffer" -- buffer completions
-  --use "hrsh7th/cmp-path" -- path completions
-  --use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
   --use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use('hrsh7th/cmp-nvim-lsp')
+  use('hrsh7th/cmp-nvim-lua')
 
   -- snippets
   use({ 'L3MON4D3/LuaSnip', run = 'make install_jsregexp', tag = 'v1.*' })
