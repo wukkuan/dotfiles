4ec392b4c859f8050ab2d01eb51b8eb8fe3663d6
William Blasko <wblasko@twitter.com>
2022-10-23 18:02:43 -0700

    use better colors for path in prompt

--- workspace/dotfiles/zsh/prompt.zsh
+++ workspace/dotfiles/zsh/prompt.zsh
@@ -8,4 +8,4 @@ autoload -Uz colors && colors
 source "$(dirname "$0")/git-prompt.sh"
 
 local NEWLINE=$'\n'
-PROMPT='${NEWLINE}$fg[blue]%n@%m$reset_color $bg[green] %~ $reset_color$fg[yellow]$(__git_ps1 " (%s)")$reset_color ${NEWLINE}# '
+PROMPT='${NEWLINE}$fg[blue]%n@%m$reset_color $bg[white]$fg[black] %~ $reset_color$fg[yellow]$(__git_ps1 " (%s)")$reset_color ${NEWLINE}# '
