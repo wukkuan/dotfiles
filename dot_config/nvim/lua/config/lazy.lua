c388067f552db96f6351d59a0d068662d1080380
William Blasko <william@williamblasko.com>
2023-12-10 14:42:14 -0800

    fix lazy.lua a bit, disable all of my plugins for now

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -54,13 +54,13 @@ require('lazy').setup({
     -- add LazyVim and import its plugins
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
     -- import any extras modules here
-    { import = 'lazyvim.plugins.extras.lang.typescript' },
-    { import = 'lazyvim.plugins.extras.lang.json' },
-    { import = 'lazyvim.plugins.extras.coding.yanky' },
-    { import = 'lazyvim.plugins.extras.formatting.prettier' },
-    { import = 'lazyvim.plugins.extras.ui.mini-animate' },
+    --{ import = 'lazyvim.plugins.extras.lang.typescript' },
+    --{ import = 'lazyvim.plugins.extras.lang.json' },
+    --{ import = 'lazyvim.plugins.extras.coding.yanky' },
+    --{ import = 'lazyvim.plugins.extras.formatting.prettier' },
+    --{ import = 'lazyvim.plugins.extras.ui.mini-animate' },
     -- import/override with your plugins
-    { import = 'plugins' },
+    --{ import = 'plugins' },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
